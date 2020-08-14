8fe1771a60db3206987631389ca05618c413af70
Juan Carlos Corona Romero <c@rlos.email>
2020-08-14 02:25:52 -0700

    feat: add metadata processing

--- package.js
+++ package.js
@@ -12,7 +12,11 @@ class MixinBuilder {
   }
 }
 
-const namespaceMap = { opf: 'http://www.idpf.org/2007/opf' }
+const namespaceMap = {
+  xml: 'http://www.w3.org/XML/1998/namespace',
+  opf: 'http://www.idpf.org/2007/opf',
+  dc: 'http://purl.org/dc/elements/1.1/',
+}
 
 const selectAll = xpath.useNamespaces(namespaceMap)
 const select = function (expression, node) {
@@ -32,7 +36,7 @@ class Node {
     return selectAll(expression, this.node)
   }
 
-  selectAttr(expression) {
+  readAttribute(expression) {
     const node = this.select(expression)
     if (node) {
       return node.value
@@ -52,23 +56,118 @@ class Node {
   }
 }
 
-let ID = (superclass) =>
+const ID = (superclass) =>
   class extends superclass {
     id() {
-      return this.selectAttr('./@id')
+      return this.readAttribute('./@id')
     }
   }
 
-let Properties = (superclass) =>
+const Refines = (superclass) =>
+  class extends superclass {
+    refines() {
+      return this.readAttribute('./@refines')
+    }
+  }
+
+const Value = (superclass) =>
+  class extends superclass {
+    value() {
+      const textNode = this.select('./text()')
+      if (textNode) {
+        return textNode.data
+      }
+    }
+  }
+
+const I18n = (superclass) =>
+  class extends superclass {
+    dir() {
+      return this.readAttribute('./@dir')
+    }
+
+    lang() {
+      return this.readAttribute('./@xml:lang')
+    }
+  }
+
+const PropertiesList = (superclass) =>
   class extends superclass {
     properties() {
-      const properties = this.selectAttr('./@properties')
+      const properties = this.readAttribute('./@properties')
       if (properties) {
         //normalize spaces and split space separated words
         return properties.replace(/\s+/g, ' ').split(' ')
       }
     }
   }
+
+const MetadataNode = (superclass) =>
+  class extends superclass {
+    constructor(node, metadata) {
+      super(node)
+      this.metadata = metadata
+    }
+  }
+
+const MetaAttributes = (superclass) =>
+  class extends superclass {
+    property() {
+      return this.readAttribute('./@property')
+    }
+
+    scheme() {
+      return this.readAttribute('./@scheme')
+    }
+  }
+
+const createMetaOrNull = (context, property) => {
+  const id = context.id()
+  const propertyMap = context.metadata.metaPropertyMap[id]
+  if (!propertyMap) {
+    return null
+  }
+
+  const metaNode = propertyMap[property]
+  if (!metaNode) {
+    return null
+  }
+
+  return new Meta(metaNode, context.metadata)
+}
+
+const MetaProperties = (superclass) =>
+  class extends superclass {
+    alternateScript() {
+      return createMetaOrNull(this, 'alternate-script')
+    }
+
+    displaySeq() {
+      return createMetaOrNull(this, 'display-seq')
+    }
+
+    fileAs() {
+      return createMetaOrNull(this, 'file-as')
+    }
+
+    groupPosition() {
+      return createMetaOrNull(this, 'group-position')
+    }
+
+    metaAuth() {
+      return createMetaOrNull(this, 'meta-auth')
+    }
+  }
+
+class Meta extends mix(Node).with(
+  MetadataNode,
+  ID,
+  Value,
+  I18n,
+  Refines,
+  MetaAttributes,
+  MetaProperties
+) {}
 
 const toArray = (valueOrArray) =>
   Array.isArray(valueOrArray) ? valueOrArray : [valueOrArray]
@@ -103,11 +202,11 @@ class Spine extends mix(Node).with(ID) {
   }
 
   pageProgressionDirection() {
-    return this.selectAttr('./@page-progression-direction')
+    return this.readAttribute('./@page-progression-direction')
   }
 
   toc() {
-    const idref = this.selectAttr('./@toc')
+    const idref = this.readAttribute('./@toc')
     if (idref) {
       return toArray(this.manifest.item({ id: idref }))[0]
     }
@@ -139,21 +238,21 @@ class Spine extends mix(Node).with(ID) {
   }
 }
 
-class SpineItem extends mix(Node).with(ID, Properties) {
+class SpineItem extends mix(Node).with(ID, PropertiesList) {
   constructor(node, spine) {
     super(node)
     this.spine = spine
   }
 
   idref() {
-    const idref = this.selectAttr('./@idref')
+    const idref = this.readAttribute('./@idref')
     if (idref) {
       return toArray(this.spine.manifest.item({ id: idref }))[0]
     }
   }
 
   linear() {
-    const linear = this.selectAttr('./@linear')
+    const linear = this.readAttribute('./@linear')
     if (linear === 'no') {
       return false
     }
@@ -161,29 +260,29 @@ class SpineItem extends mix(Node).with(ID, Properties) {
   }
 }
 
-class ManifestItem extends mix(Node).with(ID, Properties) {
+class ManifestItem extends mix(Node).with(ID, PropertiesList) {
   constructor(node, manifest) {
     super(node)
     this.manifest = manifest
   }
 
   href() {
-    return this.selectAttr('./@href')
+    return this.readAttribute('./@href')
   }
 
   mediaType() {
-    return this.selectAttr('./@media-type')
+    return this.readAttribute('./@media-type')
   }
 
   mediaOverlay() {
-    const idref = this.selectAttr('./@media-overlay')
+    const idref = this.readAttribute('./@media-overlay')
     if (idref) {
       return toArray(this.manifest.item({ id: idref }))[0]
     }
   }
 
   fallback() {
-    const idref = this.selectAttr('./@fallback')
+    const idref = this.readAttribute('./@fallback')
     if (idref) {
       return toArray(this.manifest.item({ id: idref }))[0]
     }
@@ -208,9 +307,78 @@ class Manifest extends mix(Node).with(ID) {
   }
 }
 
-export class Package extends Node {
+class Identifier extends mix(Node).with(
+  MetadataNode,
+  ID,
+  Value,
+  MetaProperties
+) {
+  identifierType() {
+    return createMetaOrNull(this, 'identifier-type')
+  }
+}
+
+class Metadata extends Node {
+  constructor(node) {
+    super(node)
+
+    this.metaPropertyMap = {}
+
+    const metaRefiningNodes = toArray(
+      this.selectAll('./opf:meta[@refines and @property]')
+    )
+
+    metaRefiningNodes.forEach((meta) => {
+      const refinesAttr = select('./@refines', meta)
+      if (!refinesAttr) {
+        return
+      }
+      const refinesValue = refinesAttr.value
+
+      // drop the # prefix
+      const idRefined =
+        refinesValue[0] === '#' ? refinesValue.substr(1) : refinesValue
+
+      const propertyAttr = select('./@property', meta)
+      if (!propertyAttr) {
+        return
+      }
+      const property = propertyAttr.value
+
+      if (!this.metaPropertyMap[idRefined]) {
+        this.metaPropertyMap[idRefined] = {}
+      }
+
+      this.metaPropertyMap[idRefined][property] = meta
+    })
+  }
+
+  identifier() {
+    const node = this.select('./dc:identifier')
+    return new Identifier(node, this)
+  }
+}
+
+export class Package extends mix(Node).with(ID, I18n) {
   constructor(doc) {
     super(select('/opf:package', doc))
+  }
+
+  version() {
+    return this.readAttribute('./@version')
+  }
+
+  uniqueIdentifier() {
+    return null
+  }
+
+  releaseIdentifier() {
+    return null
+  }
+
+  metadata() {
+    const node = this.select('./opf:metadata')
+    return new Metadata(node)
   }
 
   spine() {
